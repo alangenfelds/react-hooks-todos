56f45d8c295ebd13472a4c79c0c4fa91b8dd7ff4
Artur Lan <alangenfeld@gmail.com>
2019-05-04 13:29:20 +0300

    reducer example with counter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext, useReducer } from "react";
+import { UserContext } from "./index";
 
-function App() {
+const initialState = {
+  count: 0
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "increment":
+      return {
+        count: state.count + 1
+      };
+    case "decrement":
+      return {
+        count: state.count - 1
+      };
+    case "reset":
+    default:
+      return initialState;
+  }
+}
+
+export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const value = useContext(UserContext);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto shadow rounded">
+      <p>Hello, {value}</p>
+      <button
+        className="border rounded bg-teal m-3 p-1"
+        onClick={() => {
+          dispatch({ type: "increment" });
+        }}
+      >
+        Increment
+      </button>
+      <button
+        className="border rounded bg-teal m-3 p-1"
+        onClick={() => {
+          dispatch({ type: "decrement" });
+        }}
+      >
+        Decrement
+      </button>
+      <button
+        className="border rounded bg-orange-light m-3 p-1"
+        onClick={() => {
+          dispatch({ type: "reset" });
+        }}
+      >
+        Reset
+      </button>
+      <h2 className="text-bold ml-3">Count: {state.count}</h2>
     </div>
   );
 }
-
-export default App;
